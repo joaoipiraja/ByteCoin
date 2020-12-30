bf13f496c53b7e2fc1020b409e8dc8fcc8a8d7f2
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2020-12-30 20:13:29 -0300

    Initial Commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by João Victor Ipirajá de Alencar on 30/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
